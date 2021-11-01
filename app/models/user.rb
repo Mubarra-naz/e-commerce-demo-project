50044c78906fcd2741a37814c5cd6cb62eef974d
Mubarra-naz <mubarra.naz@square63.org>
2021-11-01 16:20:33 +0500

    lost password fucntionality using either username or email done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   devise :database_authenticatable, :registerable,
       :recoverable, :rememberable, :validatable, :confirmable
 
@@ -14,16 +13,18 @@ class User < ApplicationRecord
     @login || self.username || self.email
   end
 
-  def self.find_for_database_authentication warden_condition
-    conditions = warden_condition.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
+  private
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    users = where(conditions)
+    return users.find_by(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]) if login
+    return users.first if conditions[:username].blank?
+
+    find_by(username: conditions[:username])
   end
 
-  private
   def password_validation
     rules = {
       'must contain at least one lowercase letter'  => /[a-z]+/,
@@ -40,5 +41,4 @@ class User < ApplicationRecord
   def set_username
     self.username = self.email.split('@').first
   end
-
 end

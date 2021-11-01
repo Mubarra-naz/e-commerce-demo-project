8385158674b99ea16a19add9d8cd2d88ad820c6b
Mubarra-naz <mubarra.naz@square63.org>
2021-11-01 22:31:56 +0500

    removed errors and omissions after PR3 review 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,13 +14,12 @@ class User < ApplicationRecord
 
   private
 
-  def self.find_for_database_authentication warden_condition
+   def self.find_for_database_authentication warden_condition
     conditions = warden_condition.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
+    login = conditions.delete(:login)
+    users = where(conditions)
+    return users.find_by(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]) if login
+    return users.first if conditions[:username] || conditions[:email]
   end
 
   def password_validation

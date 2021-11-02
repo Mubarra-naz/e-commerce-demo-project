bdad5d77a8c38c586081c8144b652c61d795fcf7
Mubarra-naz <mubarra.naz@square63.org>
2021-11-02 16:54:13 +0500

    Full_name added and indentation further resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,28 @@ class User < ApplicationRecord
       :recoverable, :rememberable, :validatable, :confirmable
 
   attr_writer :login
+  attr_accessor :full_name
 
   validate :password_validation
-  validates :firstname,:lastname, presence: true, format: { with: /^[a-zA-Z]{3,30}/, multiline: true, message: "should be either uppercase or lowercase alphabets only" }
+  validates :firstname, :lastname, presence: true, format: { with: /^[a-zA-Z]{3,30}/, multiline: true, message: "should be either uppercase or lowercase alphabets only" }
   validates :username, uniqueness: true, presence: true, format: { with: /^(?=.*[a-z])|(?=.*[A-Z])|(?=.*[0-9])(?=.{3,30})/, multiline: true, message: "should be either uppercase, lowercase or numeric value" }
 
   def login
     @login || self.username || self.email
   end
 
+  def full_name
+    "#{self.firstname} #{self.lastname}"
+  end
+
   private
 
-   def self.find_for_database_authentication warden_condition
+  def self.find_for_database_authentication warden_condition
     conditions = warden_condition.dup
     login = conditions.delete(:login)
     users = where(conditions)
     return users.find_by(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]) if login
-    return users.first if conditions[:username] || conditions[:email]
+    users.first if conditions[:username] || conditions[:email]
   end
 
   def password_validation

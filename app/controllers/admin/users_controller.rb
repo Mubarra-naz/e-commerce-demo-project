0adf9bde6fe85a6ccc6e6dfc8360937fbe9fbc6b
Mubarra-naz <mubarra.naz@square63.org>
2021-11-03 17:20:08 +0500

    refactoring admin controllers

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,6 +14,6 @@ class Admin::UsersController < ApplicationController
   private
 
   def check_if_admin?
-    redirect_to new_session_path unless current_user.role == 'admin'
+    redirect_to new_session_path unless current_user.role == User::ADMIN
   end
 end

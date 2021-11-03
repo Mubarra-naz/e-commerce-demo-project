ff38034c0686308848143677b8eb8f7ca3e98c0a
Mubarra-naz <mubarra.naz@square63.org>
2021-11-03 13:09:07 +0500

    proper validations and redirections for admin added

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,20 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_if_admin?
+
   def index
     @users=User.all
+  end
+
+  def destroy
+   @user=User.find(params[:id])
+    @user.destroy
+    redirect_to admin_root_path
+  end
+
+  private
+
+  def check_if_admin?
+    redirect_to new_session_path unless (current_user.role=='admin')
   end
 end

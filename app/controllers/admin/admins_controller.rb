994347665765712ecabd311fdcee82645833afbc
Mubarra-naz <mubarra.naz@square63.org>
2021-11-04 15:59:31 +0500

    final refactoring

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -4,6 +4,6 @@ class Admin::AdminsController < ApplicationController
   protected
 
   def authorize_admin
-    redirect_to new_session_path unless current_user.admin?
+    redirect_to new_user_session_path unless current_user.admin?
   end
 end

d96037644a3d0bba7fde8b90dfdb9bf68b8b489a
Mubarra-naz <88368759+Mubarra-naz@users.noreply.github.com>
2021-11-04 17:51:07 +0500

    Revert "Admin: Listing of Users"

--- app/controllers/admin/admins_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::AdminsController < ApplicationController
-  before_action :authenticate_user!, :authorize_admin
-
-  protected
-
-  def authorize_admin
-    redirect_to new_user_session_path unless current_user.admin?
-  end
-end

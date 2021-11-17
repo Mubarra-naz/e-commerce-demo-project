e21745a9113e7932aff5d7d6d4f2fb08773c7ede
Mubarra-naz <mubarra.naz@square63.org>
2021-11-17 11:52:36 +0500

    refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def select_options(model_name, attribute)
+    symbol = attribute.to_sym
+    model_name.classify.constantize.all.map{ |item| [item[symbol], item[:id]] }
+  end
+
+  def categories_controller
+    controller_name == 'categories'
+  end
 end
--- app/helpers/dropdown_helper.rb
+++
@@ -1,5 +0,0 @@
-module DropdownHelper
-  def select_options(model_name, attribute)
-    options_from_collection_for_select(model_name.classify.constantize.all, "id", attribute)
-  end
-end

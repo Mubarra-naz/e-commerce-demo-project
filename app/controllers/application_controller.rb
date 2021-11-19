755cd64ffc425008f0e03d7a58b0e202fd127c78
Mubarra-naz <mubarra.naz@square63.org>
2021-11-19 13:39:30 +0500

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    attributes = [:firstname, :lastname, :email, :username, :password, :password_confirmation]
+    attributes = %i[firstname lastname email username password password_confirmation]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[login, password, remember_me])
   end
 
   def after_sign_in_path_for(resource)

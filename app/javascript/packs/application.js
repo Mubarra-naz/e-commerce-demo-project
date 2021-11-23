c982edb44fa9bccf4ad715f746102dc90f901ffb
Mubarra-naz <mubarra.naz@square63.org>
2021-11-23 16:27:19 +0500

    refactor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import 'bootstrap';
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
-
 require("trix");
 require("@rails/actiontext");
 require("channels");
